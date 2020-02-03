4d2b1988669485d89c56a44ab855ba86f65142bb
Saif Jamal <saifjamal4649@gmail.com>
2020-02-03 02:49:19 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,18 @@
+# def square_array(array)
+#   # your code here
+#   arr = []
+#   while array.each do |i|
+#     i**2
+# end
+# end
+
 def square_array(array)
-  # your code here
-end
+  arr = []
+  count = 0 
+  while array[count] do 
+    arr << array[count]**2 
+    count +=1 
+  end
+  return arr 
+end 
+    
